3c77da38d1fe9b1152d205b952683b1bab9da278
roiyraz88 <roiyraz88@gmail.com>
2024-10-14 01:12:57 +0300

    Added google maps feature to place item

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,13 +1,17 @@
 import React from "react";
 import UserList from "../components/UsersList";
+import Map from "../../shared/components/UIElements/Map";
+
+
 
 function Users(){
+    
 
     const USERS = [
         {
         id: 'u1',
         name: 'Roie Raz',
-        image: 'https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/434712687_25533154849608920_4942308445496564261_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=hWuki_TN91gQ7kNvgG1UAmk&_nc_ht=scontent.ftlv1-1.fna&_nc_gid=Ad7yaS5HQ1uFIh04dl1Lohb&oh=00_AYAXxK3rTTveJ5AiU8QqOFqCRjcNRBXVuMaOBhMDK1Kaeg&oe=66F866FB',
+        image: 'https://static.wixstatic.com/media/c05dc9_e4b5b2d556c449a1918b627db525b279~mv2.jpg',
         places: 3
     }
 ]; 
